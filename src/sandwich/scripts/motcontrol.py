cb12578211326e845584ffcb47e0055fca4d34d1
Simone Tolomei <tolomei.simone5@gmail.com>
2021-09-25 13:02:20 +0200

    corrected bug in motcontrol updated launchfiles updated tags config updated Bacon

--- src/sandwich/scripts/motcontrol.py
+++ src/sandwich/scripts/motcontrol.py
@@ -7,9 +7,13 @@ import struct
 import Jetson.GPIO as GPIO
 import numpy as np
 
-# Parameter to scale the motors speed.
-global lin_gain
-global ang_gain
+from rospy.core import NullHandler
+
+# Parameters to scale the motors speed.
+lin_gain = 100
+ang_gain = 100
+
+dead_zone = 12
 
 # Connection to the RPI Pico
 pico = serial.Serial(
@@ -22,45 +26,50 @@ pico = serial.Serial(
 
 
 def callback(velMsg):
-	# Log the requested value (DEBUG)
-	#rospy.loginfo("topic value:" +str(velMsg.linear.x)+str(velMsg.angular.z))
-	
 	# Compute the desired speed for each motor, using the gains
 	vdx = (velMsg.linear.x)*lin_gain		# Using the linear velocity
 	vsx = vdx
 	vdx += velMsg.angular.z*ang_gain		# And the angular velocity
 	vsx -= velMsg.angular.z*ang_gain
 	
-	vsx = vsx + np.sign(vsx)*12
-	vdx = vdx + np.sign(vdx)*12	
+	vsx = vsx + np.sign(vsx)*dead_zone				# Add offset to compensate dead zone
+	vdx = vdx + np.sign(vdx)*dead_zone
+
 	vsx = min(100, max(-100, vsx))			#Constrain the values between 100% and -100%
-	vdx = min(100, max(-100, vdx))
-	#print([vsx,vdx])
-	#rospy.loginfo("requested: "+str(vdx)+" "+str(vsx)) #Log the computed value
+	vdx = min(100, max(-100, vdx))	
+	# rospy.loginfo("requested vdx=%d, vsx=%d", vdx, vsx)
 	
 	pico.write('s'.encode()) 				# 's' is the start character for code running in the PICO
-	pico.write(struct.pack("b",vsx))		# Sends left and right speed, encoding in 1 byte signed
-	pico.write(struct.pack("b",-vdx))	
+	pico.write(struct.pack("b",-vdx))		# Sends left and right speed, encoding in 1 byte signed
+	pico.write(struct.pack("b",vsx))		# (send minus for wiring compatibility)
 
 
 def stop():
-	pico.write('s'.encode()) 				# 's' is the start character for code running in the PICO
+	pico.write('s'.encode()) 			# 's' is the start character for code running in the PICO
 	pico.write(struct.pack("b",0))		# Sends left and right speed, encoding in 1 byte signed
 	pico.write(struct.pack("b",0))
+
+def debugPICO(event):
+	while pico.readable():
+		rospy.loginfo(pico.readline())
 
 
 if __name__ == '__main__':
 	# Use GPIO 7 to signal the PICO to start driving the motors
 	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(7, GPIO.OUT, initial=GPIO.HIGH)
+	GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
 
 	rospy.init_node('motcontrol', anonymous=False)		# ROS node init
-	lin_gain = rospy.get_param('~lin_gain', 90)			# parameters init
+	lin_gain = rospy.get_param('~lin_gain', 50)			# parameters init
 	ang_gain = rospy.get_param('~ang_gain', 50)
 	rospy.loginfo("%s is %s", rospy.resolve_name('~lin_gain'), lin_gain)	# log parameters value, using the resolve_name trick
 	rospy.loginfo("%s is %s", rospy.resolve_name('~ang_gain'), ang_gain) 
 	rospy.Subscriber("cmd_vel", Twist, callback)		# subscribe to topic
-	rospy.loginfo("started") 
-	rospy.on_shutdown(stop)
-	rospy.spin()
 
+	rospy.on_shutdown(stop)
+	rospy.loginfo("motcontrol started") 
+	
+	GPIO.output(7,GPIO.HIGH)
+
+	rospy.Timer(rospy.Duration(0.1), debugPICO)
+	rospy.spin()
--- src/sandwich_autonomous/scripts/apriltag_follow.py
+++ src/sandwich_autonomous/scripts/apriltag_follow.py
@@ -5,34 +5,34 @@ from apriltag_ros.msg import AprilTagDetectionArray
 from tf.transformations import euler_from_quaternion, quaternion_matrix
 import numpy as np
 
-err_int = 0
-err = 0
-found = False
-linvel = 0.0
-fwd_err = 0.0
-fwd_speed = 0.0
 
-fwd_err_int = 0.0
+fwd_speed = 0.0
+ang_speed = 0.0
+
 
 DELTA_T = 0.06
 TARGET_DIST = 0
 TAG_ID = 2
 
-K1 = 0.6
-K2 = 1
+K1 = 1.3
+K2 = 0.001
 LAMBDA = 1
+
+MAX_FWD_SPEED = 0.35
+MAX_ANG_SPEED = 0.35
+
 def callback(out):
-	global err
-	global found
-	global size
+	global fwd_speed
+	global ang_speed
+	twist_msg = Twist()
 	for tag in out.detections:
 		if tag.id[0] == TAG_ID:
-			twist_msg = Twist()
+
 			# Pose in camera frame
 			pose_list = tag.pose.pose.pose.position
 			tag_pose = np.matrix([pose_list.x,pose_list.y, pose_list.z]).T
-			cam2wheel= np.matrix([0,0.04,-0.145]).T
-			tag_pose += cam2wheel
+			wheel_2_cam= np.matrix([0,-0.04,0.145]).T
+			tag_pose += wheel_2_cam
 			tag_pose[1] = 0
 
 			
@@ -40,46 +40,59 @@ def callback(out):
 			q = tag.pose.pose.pose.orientation
 			q_list = [q.x, q.y, q.z, q.w]
 			(roll, pitch, yaw) = euler_from_quaternion(q_list)
+
 			Rz = np.matrix([[np.cos(yaw), -np.sin(yaw), 0],[np.sin(yaw), np.cos(yaw), 0], [0,0,1]])
 			Ry = np.matrix([[np.cos(pitch), 0, np.sin(pitch)], [0,1,0],[-np.sin(pitch), 0, np.cos(pitch)]])
 			Rx = np.matrix([[1,0,0], [0, np.cos(roll), -np.sin(roll)], [0, np.sin(roll), np.cos(roll)]])
 			R = (Rz*Ry)*Rx
-			goal_vect = np.dot(R,np.array([0,0,-1])).T
+
+			goal_vect = R*np.matrix([0,0,-1]).T
 			goal_vect[1] = 0
+
+			tag_offset = np.matrix([0,0,0.02]).T
+			#tag_pose += R*tag_offset
+			tag_pose[1] = 0
+
 			theta = np.arctan2(-goal_vect[0],goal_vect[2])
 
-			tag_offset = np.array([0,0,0.25])
-			print(tag_pose)
-			print(tag_offset)
-			print(np.dot(R,tag_offset))
-			#tag_pose += np.dot(R,tag_offset)
 			
 			
 			rho = np.linalg.norm(tag_pose,2)
 
 			alpha = np.arctan2(-tag_pose[0], tag_pose[2])
 			
-			psi = alpha - theta
-
+			psi = -(alpha - theta)
+			rospy.loginfo(rho)
+			
 			# LYAPUNOV CONTROL
 			fwd_speed = K1*rho*np.cos(alpha)
+
 			ang_speed = K1*np.cos(alpha)*np.sin(alpha)*(1- psi/(LAMBDA*alpha)) + alpha*K2/LAMBDA
 			#print([rho,alpha,psi])
-			print([fwd_speed, ang_speed])
+			#print([fwd_speed, ang_speed])
 			
 
-			if rho < 0.05:
+			if rho < 0.1:
 				fwd_speed = 0.0
 				ang_speed = 0.0
-			fwd_speed = max(-0.3,min(0.3,fwd_speed))
-			ang_speed = max(-0.2, min(0.2,ang_speed))
+			fwd_speed = max(-MAX_FWD_SPEED,min(MAX_FWD_SPEED,fwd_speed))
+			ang_speed = max(-MAX_ANG_SPEED, min(MAX_ANG_SPEED,ang_speed))
 
-			twist_msg.linear.x = fwd_speed
-			twist_msg.angular.z = ang_speed
-			pub.publish(twist_msg)
-			
-			found = False
+	s = np.shape(out.detections)
+	if s[0] == 0:
+		fwd_speed = 0.8*fwd_speed
+		ang_speed = 0.8*ang_speed
 
+	twist_msg.linear.x = fwd_speed
+	twist_msg.angular.z = ang_speed
+	pub.publish(twist_msg)
+
+def stop():
+	twist_msg = Twist()
+	twist_msg.linear.x = 0.0
+	twist_msg.angular.z = 0.0
+	pub.publish(twist_msg)
+	pub.publish(twist_msg)
 
 
 if __name__ == '__main__':
@@ -88,6 +101,8 @@ if __name__ == '__main__':
 
 	rospy.Subscriber("/tag_detections", AprilTagDetectionArray, callback)
 	pub = rospy.Publisher("cmd_vel",Twist,queue_size=5)
+	
+	rospy.on_shutdown(stop)
 
 	rospy.loginfo("started")
 	rospy.spin()
